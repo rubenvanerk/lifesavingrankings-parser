b1f6eb9a050b66980a5d55a07efacc61e235a4e5
Ruben van Erk <ruben@rubenvanerk.nl>
2020-06-25 20:47:54 +0200

    Shift fixes

--- config/database.php
+++ config/database.php
@@ -2,6 +2,41 @@
 
 use Illuminate\Support\Str;
 
+$connections = [];
+
+if (ENV('DB_HOST_LOCAL')) {
+    $connections['local'] = [
+        'driver' => 'pgsql',
+        'host' => env('DB_HOST_LOCAL'),
+        'port' => env('DB_PORT_LOCAL',),
+        'database' => env('DB_DATABASE_LOCAL'),
+        'username' => env('DB_USERNAME_LOCAL'),
+        'password' => env('DB_PASSWORD_LOCAL'),
+    ];
+}
+
+if (ENV('DB_HOST_STAGING')) {
+    $connections['staging'] = [
+        'driver' => 'pgsql',
+        'host' => env('DB_HOST_STAGING'),
+        'port' => env('DB_PORT_STAGING',),
+        'database' => env('DB_DATABASE_STAGING'),
+        'username' => env('DB_USERNAME_STAGING'),
+        'password' => env('DB_PASSWORD_STAGING'),
+    ];
+}
+
+if (ENV('DB_HOST_PRODUCTION')) {
+    $connections['production'] = [
+        'driver' => 'pgsql',
+        'host' => env('DB_HOST_PRODUCTION'),
+        'port' => env('DB_PORT_PRODUCTION',),
+        'database' => env('DB_DATABASE_PRODUCTION'),
+        'username' => env('DB_USERNAME_PRODUCTION'),
+        'password' => env('DB_PASSWORD_PRODUCTION'),
+    ];
+}
+
 return [
 
     /*
@@ -15,7 +50,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'local'),
 
     /*
     |--------------------------------------------------------------------------
@@ -33,65 +68,7 @@ return [
     |
     */
 
-    'connections' => [
-
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-        ],
-
-    ],
+    'connections' => $connections,
 
     /*
     |--------------------------------------------------------------------------
--- config/filesystems.php
+++ config/filesystems.php
@@ -57,10 +57,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
+            'key' => env('S3_AWS_ACCESS_KEY_ID'),
+            'secret' => env('S3_AWS_SECRET_ACCESS_KEY'),
+            'region' => env('S3_AWS_DEFAULT_REGION'),
+            'bucket' => env('S3_AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
         ],
