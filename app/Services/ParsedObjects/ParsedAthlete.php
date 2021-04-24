4f3b1949c5f3b80cf323913a80b1655664a923df
Ruben van Erk <ruben@rubenvanerk.nl>
2021-04-24 14:37:57 +0200

    Cache found athletes

--- app/Services/ParsedObjects/ParsedAthlete.php
+++ app/Services/ParsedObjects/ParsedAthlete.php
@@ -21,6 +21,8 @@ class ParsedAthlete implements ParsedObject
     public const MALE = 1;
     public const FEMALE = 2;
 
+    protected static array $searchedAthletes;
+
     public function __construct(string $name, ?int $yearOfBirth, int $gender, ?string $nationality, ?string $team)
     {
         if (!is_null($yearOfBirth) && ($yearOfBirth < 1900 || $yearOfBirth > Carbon::now()->year)) {
@@ -33,24 +35,34 @@ class ParsedAthlete implements ParsedObject
         $this->team = $team;
     }
 
+    protected function findOrCreateAthlete(): Athlete
+    {
+        if (!isset(self::$searchedAthletes[$this->name])) {
+            $athleteSearch = Athlete::where('name', 'ilike', $this->name)->where('gender', $this->gender);
+
+            if ($this->yearOfBirth) {
+                $athleteSearch->where('year_of_birth', $this->yearOfBirth);
+            }
+
+            $athlete = $athleteSearch->first();
+
+            if (!$athlete) {
+                $athlete = Athlete::create([
+                    'name' => $this->name,
+                    'gender' => $this->gender,
+                    'year_of_birth' => $this->yearOfBirth,
+                ]);
+            }
+
+            self::$searchedAthletes[$this->name] = $athlete;
+        }
+
+        return self::$searchedAthletes[$this->name];
+    }
+
     public function saveToDatabase(): Athlete
     {
-        $athlete = Athlete::where('name', 'ilike', $this->name)
-            ->where('gender', $this->gender);
-
-        if ($this->yearOfBirth) {
-            $athlete->where('year_of_birth', $this->yearOfBirth);
-        }
-
-        $athlete = $athlete->first();
-
-        if (!$athlete) {
-            $athlete = Athlete::create([
-                'name' => $this->name,
-                'gender' => $this->gender,
-                'year_of_birth' => $this->yearOfBirth,
-            ]);
-        }
+        $athlete = $this->findOrCreateAthlete();
 
         if ($athlete->aliasOf) {
             $athlete = $athlete->aliasOf;
