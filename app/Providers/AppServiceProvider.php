cdbdc28918e4d904992dffa56f01720fb05b2ea2
Ruben van Erk <ruben@rubenvanerk.nl>
2021-05-02 09:59:52 +0200

    Use bootstrap paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useTailwind();
+        Paginator::useBootstrap();
 
         if (config('app.env') === 'dev') {
             URL::forceRootUrl(config('app.url'));
