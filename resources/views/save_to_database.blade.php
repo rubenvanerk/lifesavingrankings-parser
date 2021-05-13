470b3c79da3d17b34d101b82076401ad4c0e0616
Ruben van Erk <ruben@rubenvanerk.nl>
2021-05-13 07:33:42 +0200

    Update save to db view

--- resources/views/save_to_database.blade.php
+++ resources/views/save_to_database.blade.php
@@ -2,8 +2,16 @@
 
 @section('content')
 
-    <a href="{{route('competitions.parse', ['competition' => $competition])}}">< back to config</a> <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="{{ route('competitions.parse', ['competition' => $competition]) }}" class="btn btn-primary mb-2">
+                    Back to config
+                </a>
 
-    <h1>Competition saved</h1>
+                <h1>Competition saved</h1>
+            </div>
+        </div>
+    </div>
 
 @endsection
